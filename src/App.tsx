13697579b68ec671bc19f90360bfef375b1e2fe0
leticiafrontend <leticia.alesilva@outlook.com>
2021-07-11 13:28:09 -0300

    feat: remove component teste

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import GlobalStyle from './styles/global'
 import { DashboardContextProvider } from './context/DashboardContext'
 import { MarcasContextProvider } from './context/MarcasContext'
 import { VeiculosContextProvider } from './context/VeiculosContext'
-import { Teste } from './pages/Teste'
 
 export const App: React.FC = () => (
   <>
@@ -12,7 +11,6 @@ export const App: React.FC = () => (
     <DashboardContextProvider>
       <MarcasContextProvider>
         <VeiculosContextProvider>
-          <Teste />
           <h1>Hello world!</h1>
         </VeiculosContextProvider>
       </MarcasContextProvider>
