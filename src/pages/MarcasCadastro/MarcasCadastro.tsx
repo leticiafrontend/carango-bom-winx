3071e648e46f2285deded470e0579facd7b54826
lisandraroquebandtec <lisandra.roque@bandtec.com.br>
2021-07-12 17:21:10 -0300

    feat: add registration page

--- src/pages/MarcasCadastro/MarcasCadastro.tsx
+++ src/pages/MarcasCadastro/MarcasCadastro.tsx
@@ -1,3 +1,22 @@
 import React from 'react'
+import { Grid, TextField, Button } from '@material-ui/core'
+import { marcasCadastroStyle } from './styles'
 
-export const MarcasCadastro = () => <h1>Marcas Cadastro</h1>
+export const MarcasCadastro = () => {
+  const classes = marcasCadastroStyle()
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <form className={classes.root}>
+          <TextField label="Marca" variant="outlined" />
+          <Button size="large" variant="contained" color="primary">
+            Cadastrar
+          </Button>
+          <Button size="large" variant="contained" color="secondary">
+            Cancelar
+          </Button>
+        </form>
+      </Grid>
+    </Grid>
+  )
+}
