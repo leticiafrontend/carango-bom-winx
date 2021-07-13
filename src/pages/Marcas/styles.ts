dc65d271e6b391494c2d6cd419ccf93370dcf3d9
leticiafrontend <leticia.alesilva@outlook.com>
2021-07-13 10:20:23 -0300

    feat: adjust page marcas

---
+++ src/pages/Marcas/styles.ts
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+export const useStyles = makeStyles({
+  table: {
+    minWidth: 100,
+  },
+  title: {
+    paddingBottom: 20,
+    fontWeight: 500,
+  },
+})
