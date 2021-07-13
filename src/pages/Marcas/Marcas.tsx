038fdd97ed15253ccc8eeaa3849dcbb36ba8d723
leticiafrontend <leticia.alesilva@outlook.com>
2021-07-13 11:12:51 -0300

    feat:  add interface in page marcas

--- src/pages/Marcas/Marcas.tsx
+++ src/pages/Marcas/Marcas.tsx
@@ -17,6 +17,11 @@ import { useStyles } from './styles'
 import { useMarcas } from '../../hooks/useMarcas'
 import { deleteMarca } from '../../api/marcas'
 
+interface PropsMarcas {
+  id: number
+  nome: string
+}
+
 export const Marcas: React.FC = () => {
   const marcas = useMarcas()
   const classes = useStyles()
@@ -42,7 +47,7 @@ export const Marcas: React.FC = () => {
       <Typography className={classes.title} variant="h5" color="secondary">
         Lista de Marcas
       </Typography>
-      {marcas?.marcas?.map((p) => (
+      {marcas?.marcas?.map((marca: PropsMarcas) => (
         <div>
           <TableContainer component={Paper}>
             <Table className={classes.table} aria-label="customized table">
@@ -54,12 +59,12 @@ export const Marcas: React.FC = () => {
                       component="th"
                       scope="row"
                     >
-                      {p.nome}
+                      {marca.nome}
                     </TableCell>
                     <TableCell align="right">
                       <Button
                         color="primary"
-                        onClick={() => deleteMarcas(p.id)}
+                        onClick={() => deleteMarcas(marca.id)}
                         size="small"
                       >
                         Excluir
